f231774c913fff7797159e9eec96ba18416cce53
Londa Mngadi <mngadilg@gmail.com>
2025-09-17 23:02:16 +0200

    I now have an animated character right now the only animation he does is the idle,there's no way to switch animation

--- Kings_and_Pigs/Character/characters.py
+++ Kings_and_Pigs/Character/characters.py
@@ -3,108 +3,157 @@ import pygame
 # Kings
 
 class HumanKing(pygame.sprite.Sprite):
-    def __init__(self,dt):
+    def __init__(self,screen,pos):
         super().__init__()
-        self.states = [
-            "idle",
-            "run",
-            "jump",
-            "attack",
-            "enter",
-            "exit",
-            "hit",
-            "dead"
-        ]
+        self.screen = screen
+        self.states = ["idle","run","jump","fall","land","attack","enter","exit","hit","dead"]
         self.create_animations()
-        self.state = self.state[0]
+        self.state = self.states[0]
+        self.sprite_index = 0
+        self.img = self.idle[self.sprite_index]
+        self.rect = self.img.get_rect()
+        self.rect.midbottom = pos
 
-        def animate(self):
-            if self.state == "idle":
-                # idle animation
-                pass
-            elif self.state == "run":
-                # run animation
-                pass
-            elif self.state == "jump":
-                # jump animation
-                pass
-            elif self.state == "attack":
-                # attack animation
-                pass
-            elif self.state == "enter":
-                # enter animation
-                pass
-            elif self.state == "exit":
-                # exit animation
-                pass
-            elif self.state == "hit":
-                # hit animation
-                pass
-            elif self.state == "dead":
-                # run run animation
-                pass
+    def change_state(self,index):
+        self.state = self.states[(index)]
 
+    def animate(self):
+        if self.state == "idle":
+            # idle animation
+            self.sprite_index += 0.2
+            if int(self.sprite_index) < len(self.idle):
+                self.img = self.idle[int(self.sprite_index)]
+            else:
+                self.sprite_index = 0
+                self.img = self.idle[self.sprite_index]
+        elif self.state == "run":
+            # run animation
+            self.sprite_index += 0.2
+            if int(self.sprite_index) < len(self.run):
+                self.img = self.run[int(self.sprite_index)]
+            else:
+                self.sprite_index = 0
+                self.img = self.run[self.sprite_index]
+        elif self.state == "jump":
+            # jump animation
+            if int(self.sprite_index) >= 1:
+                self.sprite_index = 0
+                self.change_state(3)
+            else:
+                self.sprite_index += 0.2
+                self.img = self.jump
+        elif self.state == "fall":
+            # fall animation
+            if int(self.sprite_index) >= 1:
+                self.sprite_index = 0
+                self.change_state(4)
+            else:
+                self.sprite_index += 0.2
+                self.img = self.fall
+        elif self.state == "land":
+            # land animation
+            if int(self.sprite_index) >= 1:
+                self.sprite_index = 0
+                self.change_state(0)
+            else:
+                self.sprite_index += 0.2
+                self.img = self.land
+        elif self.state == "attack":
+            # attack animation
+            self.sprite_index += 0.2
+            if int(self.sprite_index) < len(self.attack):
+                self.img = self.attack[int(self.sprite_index)]
+            else:
+                self.sprite_index = 0
+                self.img = self.attack[(self.sprite_index)]
+        elif self.state == "enter":
+            # enter animation
+            self.sprite_index += 0.2
+            if int(self.sprite_index) < len(self.enter):
+                self.img = self.enter[int(self.sprite_index)]
+            else:
+                self.sprite_index = 0
+                self.img = self.enter[(self.sprite_index)]
+        elif self.state == "exit":
+            # exit animation
+            self.sprite_index += 0.2
+            if int(self.sprite_index) < len(self.exit):
+                self.img = self.exit[int(self.sprite_index)]
+            else:
+                self.sprite_index = 0
+                self.img = self.exit[(self.sprite_index)]
+        elif self.state == "hit":
+            # hit animation
+            self.sprite_index += 0.2
+            if int(self.sprite_index) < len(self.hit):
+                self.img = self.hit[int(self.sprite_index)]
+            else:
+                self.sprite_index = 0
+                self.img = self.hit[(self.sprite_index)]
+        elif self.state == "dead":
+            # dead animation
+            self.sprite_index += 0.2
+            if int(self.sprite_index) < len(self.dead):
+                self.img = self.dead[int(self.sprite_index)]
+            else:
+                self.sprite_index = 0
+                self.img = self.dead[(self.sprite_index)]
 
-        def create_animations(self):
-            location = "01-King Human"
-            self.attack = []
-            self.idle = []
-            self.jump = []
-            self.run = []
-            self.hit = []
-            self.enter = []
-            self.exit = []
-            self.dead = []
-            # Create animation frames
-            for animation in self.states:
-                if animation == "idle":
-                    for i in range(0,12):
-                        try:
-                            self.idle.append(pygame.image.load(str(location + f"/Idle/{i}.png")))
-                        except:
-                            pass
-                elif animation == "attack":
-                    for i in range(0,12):
-                        try:
-                            self.idle.append(pygame.image.load(str(location + f"/Attack/{i}.png")))
-                        except:
-                            pass
-                elif animation == "dead":
-                    for i in range(0,12):
-                        try:
-                            self.idle.append(pygame.image.load(str(location + f"/Dead/{i}.png")))
-                        except:
-                            pass
-                elif animation == "enter":
-                    for i in range(0,12):
-                        try:
-                            self.idle.append(pygame.image.load(str(location + f"/Enter/{i}.png")))
-                        except:
-                            pass
-                elif animation == "exit":
-                    for i in range(0,12):
-                        try:
-                            self.idle.append(pygame.image.load(str(location + f"/Exit/{i}.png")))
-                        except:
-                            pass
-                elif animation == "hit":
-                    for i in range(0,12):
-                        try:
-                            self.idle.append(pygame.image.load(str(location + f"/Hit/{i}.png")))
-                        except:
-                            pass
-                elif animation == "jump":
-                    for i in range(0,12):
-                        try:
-                            self.idle.append(pygame.image.load(str(location + f"/Jump/{i}.png")))
-                        except:
-                            pass
-                elif animation == "run":
-                    for i in range(0,12):
-                        try:
-                            self.idle.append(pygame.image.load(str(location + f"/Run/{i}.png")))
-                        except:
-                            pass
+    def create_animations(self):
+        self.attack = []
+        self.idle = []
+        self.run = []
+        self.hit = []
+        self.enter = []
+        self.exit = []
+        self.dead = []
+        # Create animation frames
+        for animation in self.states:
+            if animation == "idle":
+                for i in range(0,11):
+                    surface = pygame.image.load(f"Assets/King_Human/Idle/{i}.png").convert_alpha()
+                    self.idle.append(surface)
+            elif animation == "attack":
+                for i in range(0,3):
+                    surface = pygame.image.load(f"Assets/King_Human/Attack/{i}.png").convert_alpha()
+                    self.attack.append(surface)
+            elif animation == "dead":
+                for i in range(0,4):
+                    surface = pygame.image.load(f"Assets/King_Human/Dead/{i}.png").convert_alpha()
+                    self.dead.append(surface)
+            elif animation == "enter":
+                for i in range(0,8):
+                    surface = pygame.image.load(f"Assets/King_Human/Enter/{i}.png").convert_alpha()
+                    self.enter.append(surface)
+            elif animation == "exit":
+                for i in range(0,8):
+                    surface = pygame.image.load(f"Assets/King_Human/Exit/{i}.png").convert_alpha()
+                    self.exit.append(surface)
+            elif animation == "hit":
+                for i in range(0,2):
+                    surface = pygame.image.load(f"Assets/King_Human/Hit/{i}.png").convert_alpha()
+                    self.hit.append(surface)
+            elif animation == "jump":
+                surface = pygame.image.load("Assets/King_Human/Jump/0.png").convert_alpha()
+                self.jump = surface
+            elif animation == "fall":
+                surface = pygame.image.load("Assets/King_Human/Jump/1.png").convert_alpha()
+                self.fall = surface
+            elif animation == "land":
+                surface = pygame.image.load("Assets/King_Human/Jump/2.png").convert_alpha()
+                self.land = surface
+            elif animation == "run":
+                for i in range(0,8):
+                    surface = pygame.image.load(f"Assets/King_Human/Run/{i}.png").convert_alpha()
+                    self.run.append(surface)
+
+    def update(self):
+        self.animate()
+        self.display()
+
+    def display(self):
+        scaled_img = pygame.transform.scale2x(self.img)
+        self.screen.blit(scaled_img, self.rect)
 
 
 class PigKing(pygame.sprite.Sprite):
--- Kings_and_Pigs/Character/main.py
+++ Kings_and_Pigs/Character/main.py
@@ -1,17 +1,25 @@
 import pygame 
 from sys import exit
+from characters import HumanKing
 
 pygame.init()
 screen = pygame.display.set_mode((800,400))
 pygame.display.set_caption("Character prototypes")
-clock = pygame.tick.Clock()
+clock = pygame.time.Clock()
+
+player = HumanKing(screen,(400, 200))
+bg = pygame.Surface((800,400))
+bg.fill("white")
 
 while True:
     clock.tick(60)
 
-    for event in pygame.event.get()
+    for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
+
+    screen.blit(bg,(0,0))
+    player.update()
 
     pygame.display.update()
