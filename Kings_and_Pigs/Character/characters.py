10b98ce21a8da8a2a76d8f3dfbb43cf2006f6629
Londa Mngadi <mngadilg@gmail.com>
2025-09-25 00:20:38 +0200

    work on character

--- Kings_and_Pigs/Character/characters.py
+++ Kings_and_Pigs/Character/characters.py
@@ -1,4 +1,5 @@
 import pygame
+from settings import HEIGHT, WIDTH
 
 # Kings
 
@@ -8,8 +9,8 @@ class HumanKing(pygame.sprite.Sprite):
         super().__init__()
         self.speed = 10
         self.screen = screen
-        self.states = ["idle","run","jump","fall","land","attack","enter","exit","hit","dead"]
-        self.animations = { "idle": [], "run": [], "jump": [], "fall": [],"land": [],"attack": [], "enter": [], "exit": [], "hit":[],"dead":[]}
+        self.states = ["idleL","idleR","runL","runR","jump","fall","land","attack","enter","exit","hit","dead"]
+        self.animations = { "idleL": [], "idleR": [], "runL": [], "runR": [], "jump": [], "fall": [],"land": [],"attack": [], "enter": [], "exit": [], "hit":[],"dead":[]}
         self.state = self.states[0]
         self.create_animations()
         self.animation_index = 0
@@ -17,6 +18,7 @@ class HumanKing(pygame.sprite.Sprite):
         self.img = self.current_animation[self.animation_index]
         self.rect = self.img.get_rect()
         self.rect.midbottom = pos
+        self.vel = 0
 
     def move_right(self):
         self.rect.centerx += 5
@@ -42,13 +44,29 @@ class HumanKing(pygame.sprite.Sprite):
     def take_damage(self):
         pass
 
+    def boundary(self):
+        if self.rect.bottom >= HEIGHT:
+            self.rect.bottom = HEIGHT;
+            self.vel = 0
+        if self.rect.right > WIDTH:
+            self.rect.right = WIDTH;
+        if self.rect.left <= 0:
+            self.rect.left = 0;
+
+    def gravity(self):
+        self.vel = self.vel + 0.2
+        self.rect.centery += self.vel
+
     def animate(self):
         self.animation_index += 0.2
-        if int(self.animation_index) < len(self.current_animation):
-            self.img = self.current_animation[int(self.animation_index)]
+        if self.state == "jump" or self.state == "fall":
+            pass
         else:
-            self.animation_index = 0
-            self.img = self.current_animation[self.animation_index]
+            if int(self.animation_index) < len(self.current_animation):
+                self.img = self.current_animation[int(self.animation_index)]
+            else:
+                self.animation_index = 0
+                self.img = self.current_animation[self.animation_index]
 
     def change_state(self,state):
         self.state = self.states[state]
@@ -58,19 +76,24 @@ class HumanKing(pygame.sprite.Sprite):
 
     def update(self):
         self.animate()
+        self.boundary()
+        self.gravity()
         self.display()
 
     def display(self):
-        scaled_img = pygame.transform.scale2x(self.img)
-        self.screen.blit(scaled_img, self.rect)
+        #scaled_img = pygame.transform.scale2x(self.img)
+        self.screen.blit(self.img, self.rect)
         
     def create_animations(self):
         # Create animation frames
         for animation in self.states:
-            if animation == "idle":
+            if animation == "idleR":
+                for i in range(0,11):
+                    self.animations["idleR"].append(pygame.transform.flip(self.animations["idleL"][i], flip_x = True, flip_y = False))
+            if animation == "idleL":
                 for i in range(0,11):
                     surface = pygame.image.load(f"Assets/King_Human/Idle/{i}.png").convert_alpha()
-                    self.animations["idle"].append(surface)
+                    self.animations["idleL"].append(surface)
             elif animation == "attack":
                 for i in range(0,3):
                     surface = pygame.image.load(f"Assets/King_Human/Attack/{i}.png").convert_alpha()
@@ -100,10 +123,15 @@ class HumanKing(pygame.sprite.Sprite):
             elif animation == "land":
                 surface = pygame.image.load("Assets/King_Human/Jump/2.png").convert_alpha()
                 self.animations["land"] = surface
-            elif animation == "run":
+            elif animation == "runL":
                 for i in range(0,8):
                     surface = pygame.image.load(f"Assets/King_Human/Run/{i}.png").convert_alpha()
-                    self.animations["run"].append(surface)
+                    self.animations["runL"].append(surface)
+            elif animation == "runR":
+                for i in range(0,8):
+                    self.animations["runR"].append(pygame.transform.flip(self.animations["runL"][i], flip_x = True, flip_y = False))
+
+
 
 
 class PigKing(pygame.sprite.Sprite):
--- Kings_and_Pigs/Character/main.py
+++ Kings_and_Pigs/Character/main.py
@@ -1,14 +1,15 @@
 import pygame 
 from sys import exit
 from characters import HumanKing
+from settings import HEIGHT, WIDTH
 
 pygame.init()
-screen = pygame.display.set_mode((800,400))
+screen = pygame.display.set_mode((WIDTH,HEIGHT))
 pygame.display.set_caption("Character prototypes")
 clock = pygame.time.Clock()
 
-player = HumanKing(screen,(400, 200))
-bg = pygame.Surface((800,400))
+player = HumanKing(screen,(100, 200))
+bg = pygame.Surface((WIDTH,HEIGHT))
 bg.fill("white")
 
 while True:
@@ -18,14 +19,26 @@ while True:
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_a or event.key == pygame.K_LEFT:
+                player.change_state(3)
+            if event.key == pygame.K_d or event.key == pygame.K_RIGHT:
+                player.change_state(2)
+            if event.key == pygame.K_w or event.key == pygame.K_SPACE or event.key == pygame.K_UP:
+                player.perform_jump()
 
-    keys = pygame.key.get_pressed()
-    if keys[pygame.K_a] or keys[pygame.K_LEFT]:
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_a or event.key == pygame.K_LEFT:
+                player.change_state(1)
+            if event.key == pygame.K_d or event.key == pygame.K_RIGHT:
+                player.change_state(0)
+
+    key_press = pygame.key.get_pressed()
+    if key_press[pygame.K_a] or key_press[pygame.K_LEFT]:
         player.move_left()
-    if keys[pygame.K_d] or keys[pygame.K_RIGHT]:
+    if key_press[pygame.K_d] or key_press[pygame.K_RIGHT]:
         player.move_right()
-    if keys[pygame.K_w] or keys[pygame.K_UP]:
-        player.perform_jump()
+
 
     screen.blit(bg,(0,0))
     player.update()
